15d0cc4a09c077337a0c1560200839b6e2e083b3
Md Mohidul Haque <haque.mdmohidul@gmail.com>
2025-11-12 01:57:35 +0100

    Debug predict_pipeline.py file.

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -10,9 +10,13 @@ class PredictPipeline:
 
     def predict(self,features):
         try:
-            model_path=os.path.join("artifacts","model.pkl")
+            #model_path=os.path.join("artifacts","model.pkl")
             #preprocessor_path=os.path.join('artifacts','preprocessor.pkl') # proprocessor.pkl
-            preprocessor_path=os.path.join('artifacts','proprocessor.pkl')
+            #preprocessor_path=os.path.join('artifacts','proprocessor.pkl')
+            
+            model_path="artifacts\model.pkl"
+            preprocessor_path='artifacts\proprocessor.pkl'
+            
             print("Before Loading.")
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
